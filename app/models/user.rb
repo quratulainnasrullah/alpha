59a297fffc3769a4f3904c81bed8bdab2cc516c0
Qurat ul Ain <quratulainnasrullah@gmail.com>
2022-08-12 18:34:29 +0500

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord 
 
+    validates :username, presence: true,
+     uniqueness: {case_sensitive: false }, 
+     length: {minimum: 3, maximum: 25}  
+
+    VAIID_EMAIL_REJEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true,
+    length: {maximum: 105}, uniqueness: {case_sensitive: false},
+    format: { with: VAIID_EMAIL_REJEX }
+
 
     
 end
