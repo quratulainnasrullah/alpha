6efbe0708fcd903054cf2d394036953f64f792b0
Qurat ul Ain <quratulainnasrullah@gmail.com>
2022-08-14 14:54:55 +0500

    added user and articles association

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_12_081319) do
+ActiveRecord::Schema.define(version: 2022_08_14_090247) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
