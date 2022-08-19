1a1156a968653230070fe90134cb782188a37cd4
Qurat ul Ain <quratulainnasrullah@gmail.com>
2022-08-19 20:00:47 +0500

    checking all the views and adding the restrictions for logged in and logged out users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+    before_action :set_user, only: [:edit, :update, :show]
+
+
     def index
 
         @users = User.paginate(page: params[:page], per_page: 5)
@@ -27,12 +30,9 @@ class UsersController < ApplicationController
     end
     def edit
 
-        @user = User.find(params[:id])
-
     end
     def update
 
-        @user = User.find(params[:id])
         if @user.update(user_params)
 
             flash[:success] = "Your account was updated successfully"
@@ -44,8 +44,6 @@ class UsersController < ApplicationController
         end
     end
     def show
-
-        @user = User.find(params[:id])
         @user_articles= @user.articles.paginate(page: params[:page], per_page: 2)
 
     end
@@ -54,6 +52,11 @@ class UsersController < ApplicationController
         params.require(:user).permit(:username, :email, :password)
     end
      
+    def set_user
+
+        @user = User.find(params[:id])
+
+    end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   resources :articles
   resources :users, except: [:new]
   get 'signup', to:'users#new'
+  get 'login', to:'sessions#new'
+  post 'login', to: "sessions#create"
+  delete 'logout', to: "sessions#destroy"
 end
