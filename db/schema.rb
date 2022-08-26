5c255fe858718ad955a3712d7c7128b9c5b27f65
Qurat ul Ain <quratulainnasrullah@gmail.com>
2022-08-26 14:33:32 +0500

    add category and article association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_22_102931) do
+ActiveRecord::Schema.define(version: 2022_08_26_091245) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
